0e8a4ff3052b966fac23b2303f1d04b48a9ebc2f
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2021-05-26 00:36:36 +0800

    Add files via upload

---
+++ s18_sourceextraction/ao2021_s18_02.py
@@ -0,0 +1,111 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2021/05/25 20:11:50 (CST) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+# importing sys module
+import sys
+
+# importing datetime module
+import datetime
+
+# importing numpy module
+import numpy.ma
+
+# importing astropy module
+import astropy.io.fits
+import astropy.stats
+
+# importing photutils module
+import photutils.segmentation
+
+# date/time
+now = datetime.datetime.now ()
+
+# constructing parser object
+desc   = 'background estimation using source detection and masking'
+parser = argparse.ArgumentParser (description=desc)
+
+# adding arguments
+parser.add_argument ('-i', '--input-file', default='', \
+                     help='input file name')
+parser.add_argument ('-t', '--threshold', type=float, default=2.0, \
+                     help='detection threshold in sigma (default: 2)')
+parser.add_argument ('-n', '--npixels', type=int, default=5, \
+                     help='minimum number of pixels for detection (default: 5)')
+parser.add_argument ('-s', '--dilate-size', type=int, default=21, \
+                     help='dilate size (default: 21)')
+parser.add_argument ('-m', '--maxiters', type=int, default=30, \
+                     help='maximum number of iterations (default: 30)')
+parser.add_argument ('-r', '--sigma-clipping', type=float, default=4.0, \
+                     help='sigma-clipping threshold in sigma (default: 4)')
+
+# command-line argument analysis
+args = parser.parse_args ()
+
+# file names
+file_input  = args.input_file
+
+# input parameters
+threshold   = args.threshold
+npixels     = args.npixels
+dilate_size = args.dilate_size
+maxiters    = args.maxiters
+rejection   = args.sigma_clipping
+
+# check of input file name
+if not (file_input[-5:] == '.fits'):
+    print ("Input file must be a FITS file.")
+    sys.exit ()
+
+# opening FITS file
+with astropy.io.fits.open (file_input) as hdu:
+    # reading header and image
+    header = hdu[0].header
+    image  = hdu[0].data
+    # if no image in PrimaryHDU, then read next HDU
+    if (header['NAXIS'] == 0):
+        header = hdu[1].header
+        image  = hdu[1].data
+
+# making source mask
+source_mask \
+    = photutils.segmentation.make_source_mask (image, threshold,
+                                               npixels=npixels,
+                                               sigclip_iters=maxiters,
+                                               dilate_size=dilate_size)
+
+# making masked array
+image_masked = numpy.ma.array (image, mask=source_mask)
+
+# sigma-clipping
+skybg_mean, skybg_median, skybg_stddev \
+    = astropy.stats.sigma_clipped_stats (image, sigma=rejection)
+
+# mode calculation using empirical formula
+skybg_mode = 3.0 * skybg_median - 2.0 * skybg_mean
+
+# printing results
+print ("#")
+print ("# background estimation using source detection and masking")
+print ("#")
+print ("# date/time = %s" % now)
+print ("#")
+print ("# input parameters")
+print ("#")
+print ("#   input file                         = %s" % file_input)
+print ("#   detection threshold                = %f sigma" % threshold)
+print ("#   min number of pixels for detection = %d pixel" % npixels)
+print ("#   dilate size                        = %d pixel" % dilate_size)
+print ("#   max number of iterations           = %d" % maxiters)
+print ("#   sigma-clipping threshold           = %f sigma" % rejection)
+print ("#")
+print ("# results")
+print ("#")
+print ("#   mean, median, mode, stddev")
+print ("#")
+print ("%f %f %f %f" % (skybg_mean, skybg_median, skybg_mode, skybg_stddev) )
