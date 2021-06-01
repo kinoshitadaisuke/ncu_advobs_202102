81c00634886288eefb4cb141242958cc6afbb749
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2021-06-02 04:43:25 +0800

    Add files via upload

---
+++ s20_limitingmagnitude/ao2021_s20_02.py
@@ -0,0 +1,143 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2021/06/01 21:56:27 (CST) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+# importing sys module
+import sys
+
+# importing datetime module
+import datetime
+
+# importing numpy module
+import numpy.ma
+
+# importing astropy module
+import astropy.convolution
+import astropy.io.fits
+import astropy.io.votable
+import astropy.stats
+
+# importing photutils module
+import photutils.segmentation
+
+# date/time
+now = datetime.datetime.now ()
+
+# constructing parser object
+desc   = 'source extraction using image segmentation and deblending'
+parser = argparse.ArgumentParser (description=desc)
+
+# adding arguments
+parser.add_argument ('-i', '--file-input', default='', \
+                     help='input FITS file name')
+parser.add_argument ('-o', '--file-output', default='', \
+                     help='output VOTable file name')
+parser.add_argument ('-t', '--threshold', type=float, default=3.0, \
+                     help='detection threshold in sigma (default: 3)')
+parser.add_argument ('-u', '--threshold-for-sky', type=float, default=2.0, \
+                     help='detection threshold for sky estimate (default: 2)')
+parser.add_argument ('-n', '--npixels', type=int, default=5, \
+                     help='minimum number of pixels for detection (default: 5)')
+parser.add_argument ('-s', '--dilate-size', type=int, default=21, \
+                     help='dilate size (default: 21)')
+parser.add_argument ('-m', '--maxiters', type=int, default=30, \
+                     help='maximum number of iterations (default: 30)')
+parser.add_argument ('-r', '--sigma-clipping', type=float, default=4.0, \
+                     help='sigma-clipping threshold in sigma (default: 4)')
+parser.add_argument ('-k', '--gaussian-fwhm', type=float, default=3.0, \
+                     help='Gaussian FWHM in pixel for convolution (default: 3)')
+parser.add_argument ('-a', '--kernel-size', type=int, default=3, \
+                     help='Gaussian kernel array size in pixel (default: 3)')
+
+# command-line argument analysis
+args = parser.parse_args ()
+
+# file names
+file_input  = args.file_input
+file_output = args.file_output
+
+# input parameters
+threshold         = args.threshold
+threshold_for_sky = args.threshold
+npixels           = args.npixels
+dilate_size       = args.dilate_size
+maxiters          = args.maxiters
+rejection         = args.sigma_clipping
+gaussian_fwhm     = args.gaussian_fwhm
+kernel_array_size = args.kernel_size
+
+# check of input file name
+if not (file_input[-5:] == '.fits'):
+    print ("Input file must be a FITS file.")
+    sys.exit ()
+
+# check of catalogue file name
+if not (file_output[-4:] == '.vot'):
+    print ("Output file must be a VOTable file.")
+    sys.exit ()
+
+# opening FITS file
+with astropy.io.fits.open (file_input) as hdu:
+    # reading header and image
+    header = hdu[0].header
+    image  = hdu[0].data
+    # if no image in PrimaryHDU, then read next HDU
+    if (header['NAXIS'] == 0):
+        header = hdu[1].header
+        image  = hdu[1].data
+
+# making source mask
+source_mask \
+    = photutils.segmentation.make_source_mask (image, threshold_for_sky,
+                                               npixels=npixels,
+                                               sigclip_iters=maxiters,
+                                               dilate_size=dilate_size)
+
+# making masked array
+image_masked = numpy.ma.array (image, mask=source_mask)
+
+# sigma-clipping
+skybg_mean, skybg_median, skybg_stddev \
+    = astropy.stats.sigma_clipped_stats (image, sigma=rejection)
+
+# mode calculation using empirical formula
+skybg_mode = 3.0 * skybg_median - 2.0 * skybg_mean
+
+# detection threshold in ADU
+threshold_adu = skybg_mode + threshold * skybg_stddev
+
+# 2D Gaussian kernel for convolution
+gaussian_sigma = gaussian_fwhm * astropy.stats.gaussian_fwhm_to_sigma
+kernel = astropy.convolution.Gaussian2DKernel (gaussian_sigma, \
+                                               x_size=kernel_array_size, \
+                                               y_size=kernel_array_size)
+kernel.normalize ()
+
+# source detection
+image_segm = photutils.segmentation.detect_sources (image, threshold_adu, \
+                                                    npixels=npixels, \
+                                                    filter_kernel=kernel)
+
+# deblending
+image_deblend = photutils.segmentation.deblend_sources (image, image_segm, \
+                                                        npixels=npixels, \
+                                                        filter_kernel=kernel, \
+                                                        nlevels=32, \
+                                                        contrast=0.001)
+
+# making a source catalogue
+catalogue = photutils.segmentation.SourceCatalog (image, image_deblend)
+
+# making a table
+table_sources = catalogue.to_table ()
+
+# converting a table into a VOTable
+votable_sources = astropy.io.votable.from_table (table_sources)
+
+# writing VOTable to a file
+astropy.io.votable.writeto (votable_sources, file_output)
